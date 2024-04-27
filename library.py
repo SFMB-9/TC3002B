1a8c1f5b36746134d1190c926994b81a1e5c3183
Firefrog88 <A01029956@ITESM.mx>
2024-04-27 01:01:09 -0600

    Add library

---
+++ library.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib
+import cv2
+
+def load_image(path):
+    path = path.strip()
+    return cv2.imread(path)
+
+def save_image(filename, image):
+    cv2.imwrite(filename, image)
+
+def show_image(img):
+    cv2.imshow('window' , img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return img
+
+def search_cv2(function_name):
+    try:
+        return getattr(cv2, function_name)
+    except Exception as e:
+        return "AttributeError: module 'cv2' has no attribute", e
+    return None
+
+def gen_matrix(a, b, *args):
+    s = np.array(args)
+    return s.reshape(int(a), int(b))
+
+def gen_vector(*args):
+    s =  np.array(args)
+    return s

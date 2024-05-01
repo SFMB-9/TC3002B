4576e98a1c9a263e77ae555d4018a78cbf427f0d
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2024-04-30 21:32:34 -0600

    Fix file reading script

--- lex_runner.py
+++ lex_runner.py
@@ -2,28 +2,28 @@ import os
 from lexer import parser, execute_parse_tree, add_node, parseGraph, draw, nx, plt
 
 def run_lex_file(file_path):
-        with open(file_path, 'r') as file:
-            data = file.read()
-            root = add_node({"type":"ROOT", "label":"ROOT"})
-            result = parser.parse(data)
-            try:
-              parseGraph.add_edge(root["counter"], result["counter"]) # Add an edge from the root node to the result node.
-            except TypeError:
-              return
-            except Exception as e:
-              print(e)
-            draw = False
-            labels = nx.get_node_attributes(parseGraph, 'label') # Get the labels of the nodes in the parse tree.
-            if (draw):
-                
-                # pos = graphviz_layout(parseGraph, prog="dot")  # commented out if on MacOs (not supported)
-                nx.draw(parseGraph,labels=labels, with_labels=True, font_weight='bold') # pos=pos)
-                plt.show()
+  with open(file_path, 'r') as file:
+    data = file.read()
+    root = add_node({"type":"ROOT", "label":"ROOT"})
+    result = parser.parse(data)
+    try:
+      parseGraph.add_edge(root["counter"], result["counter"]) # Add an edge from the root node to the result node.
+    except TypeError:
+      return
+    except Exception as e:
+      print(e)
+    draw = False
+    labels = nx.get_node_attributes(parseGraph, 'label') # Get the labels of the nodes in the parse tree.
+    if (draw):
+        
+        # pos = graphviz_layout(parseGraph, prog="dot")  # commented out if on MacOs (not supported)
+        nx.draw(parseGraph,labels=labels, with_labels=True, font_weight='bold') # pos=pos)
+        plt.show()
 
-        execute_parse_tree(parseGraph)
-
+  execute_parse_tree(parseGraph)
 
 def main():
+  while True:
     # Print a menu to choose file to be read
     examples_path = "examples/"
     os.system('cls')
@@ -34,18 +34,24 @@ def main():
     for i, file in enumerate(files):
       # print last file with a different format:
       padding = ' ' * (15 - len(file))
-      if i == len(files)-1:
-        print(f" └──{file}{padding} -> ({i+1})")
-      else:
-        print(f" ├──{file}{padding} -> ({i+1})")
-
+      print(f" ├── {file}{padding} -> ({i+1})")
+    print(" └── (exit)          -> (x)")
 
     # Get user input
-    file_index = int(input("> "))
-    # Read the file
+    file_index = input("> ")
+    if file_index == 'x':
+      break
+    try:
+      file_index = int(file_index)
+    except ValueError:
+      input("Invalid option . . . ")
+      continue
+    if file_index < 1 or file_index > len(files):
+      input("Invalid option . . . ")
+      continue
     file_path = examples_path + files[file_index-1]
     run_lex_file(file_path)
+    input("\nPress Enter to continue . . . ")
   
-
 if __name__ == "__main__":
-    main()
+  main()
